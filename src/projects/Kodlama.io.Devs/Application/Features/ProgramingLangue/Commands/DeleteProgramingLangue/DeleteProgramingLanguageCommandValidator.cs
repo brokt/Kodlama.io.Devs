5e11432c8aed6a9a36cd229e0a67cac9613a480e
bora.oktekin <bora.oktekin@LPTNET064>
2022-09-05 09:31:37 +0300

    Validation added

---
+++ src/projects/Kodlama.io.Devs/Application/Features/ProgramingLangue/Commands/DeleteProgramingLangue/DeleteProgramingLanguageCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgramingLangue.Commands.DeleteProgramingLangue
+{
+    public class DeleteProgramingLanguageCommandValidator : AbstractValidator<DeleteProgramingLangueCommand>
+    {
+        public DeleteProgramingLanguageCommandValidator()
+        {
+            RuleFor(r => r.Id).NotEmpty();
+        }
+    }
+}
--- src/projects/Kodlama.io.Devs/WebAPI/Program.cs
+++ src/projects/Kodlama.io.Devs/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.ConfigureCustomExceptionMiddleware();
 app.UseAuthorization();
 
 app.MapControllers();
